faecd469e6957671dc7d36fd2c4d85632bc729b4
Sanji Winaya <sanzlvr654@gmail.com>
2023-01-21 02:14:32 +0700

    admin db

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,15 @@ $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
 $routes->get('/news', 'News::index');
 $routes->get('/news/(:any)', 'News::viewNews/$1');
+
+
+$routes->group('admin', function($routes) {
+    $routes->get('news', 'NewsAdmin::index');
+    $routes->get('news/(:segment)/preview', 'NewsAdmin::preview/$1');
+    $routes->add('news/new', 'NewsAdmin::create');
+    $routes->add('news/(:segment)/edit', 'NewsAdmin::edit/$1');
+    $routes->get('news/(:segmemt)/delete', 'NewsAdmin::delete/$1');
+});
 /*
  * --------------------------------------------------------------------
  * Additional Routing
